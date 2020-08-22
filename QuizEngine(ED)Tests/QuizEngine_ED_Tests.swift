f52763c307a098cddbef2c528dd5fd5b054529c6
Averiser <averiser@gmail.com>
2020-08-22 11:50:37 +0300

    Initial Commit

---
+++ QuizEngine(ED)Tests/QuizEngine_ED_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  QuizEngine_ED_Tests.swift
+//  QuizEngine(ED)Tests
+//
+//  Created by MyMacBook on 22.08.2020.
+//  Copyright © 2020 MyMacBook. All rights reserved.
+//
+
+import XCTest
+@testable import QuizEngine_ED_
+
+class QuizEngine_ED_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
